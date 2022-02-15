2388841cd1843d090dbc06e96992c075cdb31cb7
suvi hanninen <suvh@itu.dk>
2022-02-15 23:40:17 +0100

    Added a print line on onCreate.

--- app/src/main/java/com/example/garbagesortingapp/MainActivity.java
+++ app/src/main/java/com/example/garbagesortingapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "Starting on create");
         setContentView(R.layout.activity_main);
         Intent intent= new Intent(MainActivity.this, AddNewItemActivity.class);
 
