629a33607785b2976fa2b53be93c33e3343482ce
suvi hanninen <suvh@itu.dk>
2022-04-05 23:17:01 +0200

    Added keyboard to disappear and an alret dialog

---
+++ app/src/main/java/com/example/garbagesortingapp/RecyclerViewAdapter.java
@@ -0,0 +1,16 @@
+package com.example.garbagesortingapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Map;
+
+//MyViewHolder is an inner class inside our main adaper class
+//Then we also need to extend another class in our inner class which is RecyclerView.ViewHolder
+// also we need to add a constructor to RecyclerViewAdapter
